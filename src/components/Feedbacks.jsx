74168cfcce132a28f49294eda567da690c66d7e3
Fernando <fernando@local>
2026-04-29 16:55:37 -0300

    fix: mobile video autoplay by restoring muted and playsinline

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -143,7 +143,7 @@ const Feedbacks = () => {
           >
             <iframe
               ref={iframeRef}
-              src="https://player.vimeo.com/video/1181745272?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=0&controls=0&title=0&byline=0&portrait=0&dnt=1"
+              src="https://player.vimeo.com/video/1181745272?badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&loop=1&muted=1&controls=0&playsinline=1&title=0&byline=0&portrait=0&dnt=1"
               frameBorder="0"
               allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
               loading="lazy"
